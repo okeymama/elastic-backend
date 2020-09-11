7cf1e5c2130161ef83e28fe5423a6eb0d30a10fa
cchaubey <cchaubey@deloitte.com>
2020-09-11 18:14:09 +0530

    Updated Id datatype

--- src/main/java/com/elasticbackend/search/repo/PurchaseOrderRepo.java
+++ src/main/java/com/elasticbackend/search/repo/PurchaseOrderRepo.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.elasticbackend.search.dto.PurchaseOrderDto;
 
 @Repository
-public interface PurchaseOrderRepo extends ElasticsearchRepository<PurchaseOrderDto, Long> {
+public interface PurchaseOrderRepo extends ElasticsearchRepository<PurchaseOrderDto, String> {
 
 }
